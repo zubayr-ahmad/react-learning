e5db06b762eaa18c03f859fd764779c05978026f
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-01-19 23:18:12 +0500

    completed hour 01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Capitalising the function names tell react that this is a component
+function Greeting(){
+  return <h4>This is my first component</h4>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greeting/>,document.getElementById('root'));
+
