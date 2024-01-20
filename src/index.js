e988765000b01a1ed25187bbc9dff0a44e9f00b2
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-01-20 19:41:46 +0500

    JSX CSS

--- src/index.js
+++ src/index.js
@@ -34,9 +34,9 @@ const Book = () => {
   );
 };
 const Image = () => <img src="https://m.media-amazon.com/images/I/61Wgw1NE0xL._AC_UL320_.jpg" alt="book" />;
-const Title = () => <h3>Sales Activation: Create needs and fulfill demands at the point-of-purchase</h3>;
+const Title = () => <h1>Sales Activation: Create needs and fulfill demands at the point-of-purchase</h1>;
 const Author = () => {
-  return <h2>Baldur Gudgeirsson</h2>
+  return <h4 style={{color:"#617d98",fontSize:"0.75rem",marginTop:"0.25rem"}}>Baldur Gudgeirsson</h4>
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
