ded561b2d6581f2d9382bc91ab6418a4bf2b8c78
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-01-20 17:21:43 +0500

    components return a function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDom from 'react-dom'
 
 // Capitalising the function names tell react that this is a component
 function Greeting(){
-  return <h4>This is my first component</h4>
+  return (<div>
+    <h1>This is my first component</h1>
+  </div>)
 }
 
+// const Greeting = () => {
+//   return React.createElement('div', {},React.createElement('h1',{},"this is the first component again..."))
+// }
 ReactDom.render(<Greeting/>,document.getElementById('root'));
 
