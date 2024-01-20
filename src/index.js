d92d47300ce8893ae777dfeea162282fdce4f2fd
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-01-20 21:04:43 +0500

    JSX Javascript

--- src/index.js
+++ src/index.js
@@ -8,36 +8,23 @@ function Greeting() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
-
+const author = 'Baldur Gudgeirsson'
 const Book = () => {
+  const title = "Sales Activation: Create needs and fulfill demands at the point-of-purchase"
   return (
     <article className='book'>
-      <Image />
-      <Title></Title>
-      <Author/>
+      <img src="https://m.media-amazon.com/images/I/61Wgw1NE0xL._AC_UL320_.jpg" alt="book" />
+      {/* the JSX we write should always return something. It cannot be a statement like: `let x = 6` this will give error.*/}
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <p>{6 + 6}</p>
     </article>
   );
 };
-const Image = () => <img src="https://m.media-amazon.com/images/I/61Wgw1NE0xL._AC_UL320_.jpg" alt="book" />;
-const Title = () => <h1>Sales Activation: Create needs and fulfill demands at the point-of-purchase</h1>;
-const Author = () => {
-  return <h4 style={{color:"#617d98",fontSize:"0.75rem",marginTop:"0.25rem"}}>Baldur Gudgeirsson</h4>
-}
+
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
 
