f21536c99f4760a91abe3cb7e5ed9e86e7e3ea2f
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-01-27 17:13:11 +0500

    ES6 Modules, import export

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+    {
+        author: 'Jordan Moore',
+        title: 'Interesting Facts For Curious Minds',
+        img: './Images/book1.jpg',
+        id: 1
+    },
+    {
+        author: 'James Clear',
+        title: 'Atomic Habits',
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+        id: 2
+    }
+]
+// export default books
--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 
-// CSS
 import './index.css'
-//  setup vars
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './Images/book1.jpg',
-    id: 1
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    id: 2
-  }
-]
-
+import {books} from './books'
+// import banana from './books';  // using default export
+import Book from './Book';   // using default export
 
 function BookList() {
-  function getBook(id){
-    const book_found = books.find((book) => book.id === id)
-    console.log(book_found)
-  };
-
   return (
     <section className='booklist'>
       {books.map((book) => {
         return (
-          <Book {...book} key={book.id} getBook={getBook}/>
+          <Book {...book} key={book.id}/>
         )
       })}
     </section>
@@ -38,24 +19,6 @@ function BookList() {
 }
 
 
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props
-  // console.log(props)
-  // getBook(2);
-  function getSingleBook(){
-    getBook(id)
-  };
-  return (
-    <article className='book'>
-      <img src={img} alt="book" />
-      <h1>{title}</h1>
-      {/* Passing the reference will solve the bug */}
-      {/* <button onClick={getSingleBook}>Find Book</button> */}
-      <button onClick={()=>getBook(id)}>Find Book</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(<BookList />)
