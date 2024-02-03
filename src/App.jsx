b5a337ffd5e71f3fd42e231e1ab76ca40213a643
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-03 23:18:40 +0500

    useState error-example

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Starter from "./tutorial/01-useState/starter/01-error-example";
+
 function App() {
+
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
-    </div>
+    <>
+      <Starter />
+    </>
   );
 }
 
--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,15 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+  const count_inc = () => {
+    count += 1;
+    console.log(count);
+  };
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={count_inc} className="btn">Counter</button>
+    </div>
+  );
 };
 
 export default ErrorExample;
