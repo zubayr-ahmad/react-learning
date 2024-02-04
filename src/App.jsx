b490e4f1abc0d5392a440b254769f63917d447c8
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-04 13:22:12 +0500

    useState object

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Starter from "./tutorial/01-useState/starter/03-useState-array";
-import Final from "./tutorial/01-useState/final/03-useState-array";
+import Starter from "./tutorial/01-useState/starter/04-useState-object";
+import Final from "./tutorial/01-useState/final/04-useState-object";
 
 
 function App() {
 
   return (
-    <>
+    <div>
       <Starter />
-      
-    </>
+    </div>
+    
   );
 }
 
