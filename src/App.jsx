d9d919b8a98f8191b748428862a5ae384d54e8ee
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-04 20:52:05 +0500

    conditional rendering github data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
-import Final from "./tutorial/02-useEffect/final/04-fetch-data";
-
+import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+import Final from "./tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data";
 
 function App() {
 
   return (
     <div>
       <Starter />
-      {/* <Final /> */}
+      <Final />
     </div>
     
   );
