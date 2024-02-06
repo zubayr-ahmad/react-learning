6bd7f9817cdda810d5c6748659d4f95ee4e5017e
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-06 21:31:13 +0500

    folder structure 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
-import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
-
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 function App() {
 
   return (
-    <div>
-      <Starter />
-      <Final /> 
+    <div className="container">
+      <Navbar />
     </div>
     
   );
