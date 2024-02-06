1f0cc194af6ad2d086cc920d837e7219a29934c2
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-06 20:50:37 +0500

    cleanup function 01

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [value, setValue] = useState(false);
+  console.log(value);
+  return <div>
+    <button className="btn" onClick={()=> setValue(!value)}>toggle element</button>
+    {value && <ExtraComp />}
+
+    </div>;
 };
+
+const ExtraComp = () =>{
+  useEffect(()=>{
+    console.log('Extra component mounted');
+    const intID = setInterval(() => {
+      console.log('Hello world');
+    }, 1000);
+    // return the cleanup function
+    return () => {
+      console.log('Extra component unmounted');
+      clearInterval(intID);
+    }
+
+  },[]);
+  
+  return <h2>Extra Component</h2>
+}
 
 export default CleanupFunction;
