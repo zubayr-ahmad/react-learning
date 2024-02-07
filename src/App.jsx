efcacce925b1529bc0f4666d80672a753480338d
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-07 20:25:58 +0500

    checkbox (checked) and multiple options using select

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import Start from "./tutorial/06-forms/starter/03-multiple-inputs";
-import Final from "./tutorial/06-forms/final/03-multiple-inputs";
+import Start from "./tutorial/06-forms/starter/04-other-inputs";
+import Final from "./tutorial/06-forms/final/04-other-inputs";
 function App() {
-  
   return (
     <div className="container">
       <Start />
