fefa927e420ee00808c0eff2c59b4b06f1e89f5f
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-07 20:15:03 +0500

    handling login input better way

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Start from "./tutorial/06-forms/starter/02-user-challenge";
-import Final from "./tutorial/06-forms/final/02-user-challenge";
+import Start from "./tutorial/06-forms/starter/03-multiple-inputs";
+import Final from "./tutorial/06-forms/final/03-multiple-inputs";
 function App() {
   
   return (
