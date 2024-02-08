dd0cfb0d35ddbbd362b1d37be08e1f98010de1bf
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-08 15:27:27 +0500

    useRef and getting dom nod

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -3,12 +3,19 @@ import { useEffect, useRef, useState } from 'react';
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
   const refContainer = useRef(null);
+  const isMounted = useRef(false);
+  useEffect(() => {
+    refContainer.current.focus();
+  });
   useEffect(() => {
     // Means useEffect will run at the end of render
-    // console.log(refContainer);
-    // refContainer.current.focus();
-  });
-  console.log(refContainer);
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    console.log('re-render');
+  },[value]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = refContainer.current.value;
