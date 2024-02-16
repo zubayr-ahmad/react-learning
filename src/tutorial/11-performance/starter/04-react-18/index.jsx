97b7deed118fe961eabb53ba90b1667448c79b18
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-16 23:02:37 +0500

    Lazy loading

--- src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,8 +1,12 @@
-import { useState, useTransition } from 'react';
+import { useState, useTransition, Suspense, lazy } from 'react';
+const SlowComponent = lazy(()=>import('./SlowComponent'));
+
 const LatestReact = () => {
   const [text, setText] = useState('');
   const [items, setItems] = useState([]);
   const [isPending, startTransition] = useTransition();
+  const [show, setShow] = useState(false);
+
   const handleChange = (e) => {
     setText(e.target.value);
 
@@ -39,6 +43,13 @@ const LatestReact = () => {
       >
         {items}
       </div>
+    }
+    <button className='btn' onClick={()=>setShow(!show)}>toggle</button>
+    {show && 
+    <Suspense fallback={<h4>Loading...</h4>}>
+      <SlowComponent />
+    </Suspense>
+    
     }
     </section>
   );
