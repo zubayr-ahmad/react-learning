7ef663fe1716721d2edc555be731d8b5da9cd768
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-16 21:00:01 +0500

    using memo to save re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Start from "./tutorial/11-performance/starter/02-lower-state-challenge";
+import Start from "./tutorial/11-performance/starter/03-hooks";
 // import Final from "./tutorial/07-useRef/final/01-useRef-basics";
 function App() {
   return (
