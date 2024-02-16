cf92c3d440949d5febc811bda4b27bb77754ea86
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-16 16:21:36 +0500

    global context

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useGlobalContext } from './context'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const {name} = useGlobalContext();
+  console.log(name);
   return (
     <>
       <div>
