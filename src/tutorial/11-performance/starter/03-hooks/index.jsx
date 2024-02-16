1bc75b174711b4390b859705a38f790533ecab5f
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-16 21:23:04 +0500

    useCallback, handles the rerending of function creation

--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { data } from '../../../../data';
 import List from './List';
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
-  const removePerson = (id) => {
+
+  // useCallback is used to prevent the function from being re-created on every render
+  // This is useful when passing the function to a child component. Otherwise, the 
+  // when count changes, it recreates this function and props removePerson will be
+  // changed and the child component will re-render
+  const removePerson = useCallback((id) => {
     const newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
-  };
+  },[people])
+  // have to pass people as a dependency because otherwise it will use the initial state
   return (
     <section>
       <button
